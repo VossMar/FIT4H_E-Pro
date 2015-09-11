36c9618d9560e50975b4cd5e6b53531af72e8d43
--global <vossi.93@hotmail.de>
2015-09-11 13:13:02 +0200

    ich

--- ruby/code/flicks.rb
+++ ruby/code/flicks.rb
@@ -1,16 +1,28 @@
 class Movie
- def initialize(ptitle,prank=0)
- 	@title = ptitle.capitalize
- 	@rank = prank
- end
+	def initialize(ptitle,prank=0)
+ 		@title = ptitle.capitalize
+ 		@rank = prank
+	end
 
-def to_s
-	"#{@title} hat ein Ranking von #{@rank}"
-end
- 
+	def thumbs_up
+		@rank += 1
+		#@rank = @rank +1
+	end
+
+	def thumbs_down
+		@rank -= 1
+		#@rank = @rank 1
+	end
+
+	def to_s
+		"#{@title} hat ein Ranking von #{@rank}"
+	end
+
 end
 
 
 
 obj_movie1 = Movie.new("NinjaTurtles")
+puts obj_movie1.to_s
+obj_movie1.thumbs_up
 puts obj_movie1.to_s
