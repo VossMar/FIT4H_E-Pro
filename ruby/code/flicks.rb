4b1688db0df41b65ca995de96f60c72a9aee3386
--global <vossi.93@hotmail.de>
2015-09-18 09:11:57 +0200

    füo

--- ruby/code/flicks.rb
+++ ruby/code/flicks.rb
@@ -1,18 +1,12 @@
 class Movie
+	#attr_reader :title
+	#attr_writer :title
+	attr_accessor :title
+
 	def initialize(ptitle,prank=0)
  		@title = ptitle.capitalize
  		@rank = prank
 	end
-
-	# def title
-	# 	@title
-	# end
-	attr_reader :title
-
-	# def title=(ptitle)
-	# 	@title=ptitle
-	# end
-	attr_writer :title
 
 	def thumbs_up
 		@rank += 1
@@ -31,12 +25,15 @@ class Movie
 end
 
 movie1 = Movie.new("NinjaTurtles")
+movie2 = Movie.new("45 years",8)
+movie3 = Movie.new("8mm",10)
+movie4 = Movie.new("Shooter",10)
+movie5 = Movie.new("Happy Halloween",1)
 
-puts movie1.to_s
-movie1.thumbs_up
-puts movie1.to_s
-puts movie1.title
+movies = [movie1, movie2, movie3, movie4, movie5]
+movies.each do |movie|
+	movie.thumbs_up
+	movie.thumbs_down
+	puts movie
+end
 
-movie1.title = "NinjaPurples"
-
-puts movie1
