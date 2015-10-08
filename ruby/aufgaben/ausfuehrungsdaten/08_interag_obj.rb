f277141b6ae4e430a58f0774eaf2ab3d49bac4cb
VossMar <vossi.93@hotmail.de>
2015-10-08 11:57:47 +0200

    Dostag

--- ruby/aufgaben/ausfuehrungsdaten/08_interag_obj.rb
+++ ruby/aufgaben/ausfuehrungsdaten/08_interag_obj.rb
@@ -67,7 +67,7 @@ player1 = Player.new("Marvin",90)
 player2 = Player.new("Hans")
 player3 = Player.new("Timo",60)
 
-ga = Game.new("Knucḱleheads")
+ga = Game.new("Knuckleheads")
 ga.add_player(player1)
 ga.add_player(player2)
 ga.add_player(player3)
---
+++ ruby/aufgaben/ausfuehrungsdaten/11_kredit/nr11.rb
@@ -0,0 +1,9 @@
+require_relative 'kunde'
+require_relative 'kredit'
+
+kunde1 = Kunde.new("Theo","Sommer")
+kunde1.name = "Theo Sonnenschein"
+puts kunde1.name
+kunde1.gehalt = 2000
+kunde1.adresse = "Hermelinweg 11, 22159 Hamburg"
+kunde1.adresse1
