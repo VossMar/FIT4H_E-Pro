516f7272cf54769768e01dd77f5d353ad04075cd
VossMar <vossi.93@hotmail.de>
2015-10-09 12:11:28 +0200

    sdf

--- ruby/aufgaben/ausfuehrungsdaten/11_kredit/kunde.rb
+++ ruby/aufgaben/ausfuehrungsdaten/11_kredit/kunde.rb
@@ -1,14 +1,15 @@
 class Kunde
 	attr_accessor :name
 	attr_accessor :gehalt
-	attr_accessor:adresse
+	attr_writer:adresse
 
-	def initialize(vorname,zuname)
-		@name = "#{vorname} #{zuname}"
+	def initialize(name)
+		@name = name
 	end
 
-	def adresse1
-		puts "#{@name} #{adresse}"
+	def adresse
+		@ardesse
+		"#{@name} #{@adresse}"
 	end
 
 end
@@ -20,13 +21,3 @@ end
 
 
 
-#1.  Klasse Kunde
-#1a. Legen Sie eine Eigenschaft name und adresse in der Klasse Kunde an. Für die Eigenschaft name gibt es einen Lese- und Schreibzugriff, fuer die Eigenschaft #adresse einen Schreibzugriff. 
-#b. Die Eigenschaft name enthält den Vor- und Zunamen des Kunden. Diese Eigenschaft wird beim Anlegen des Objektes übergeben. Schreiben Sie den entsprechenden #Konstruktor. 
-#c. Erzeugen Sie ein Objekt mit dem Namen 'kunde1' und dem Initialwert: 'Theo Sommer' 
-#d. Ändern Sie die Eigenschaft 'name' des Objektes in: 'Theo Sonnenschein' 
-#e. Schreiben Sie den Befehl, der den Namen auf der Konsole ausgibt. 
-#f. Fügen Sie der Klasse Kunde die Eigenschaft gehalt hinzu, die sowohl gelesen als auch geschrieben werden kann. Schreiben Sie den Befehl auf, der den Wert von #'gehalt' des Objektes auf 2000 setzt. 
-#g. Die Eigenschaft adresse bekommt folgenden String uebergeben: Hermelinweg 11, 22159 Hamburg
-#h. Schreiben Sie eine  Getter-Methode' für die Klasse Kunde, für die Eigenschaft adresse die folgende Ausgabe produziert: 'Theo Sonnenschein, Hermelinweg 11, 22159 #Hamburg.'  und geben sie das auf der Konsole aus.
-#i. Zusatz optional: Sichern Sie, dass im String Theo Sonnenschein sowohl Theo, als auch Sonnenschein jeweils mit einem Grossbuchstaben beginnen
--- ruby/aufgaben/ausfuehrungsdaten/11_kredit/nr11.rb
+++ ruby/aufgaben/ausfuehrungsdaten/11_kredit/nr11.rb
@@ -1,9 +1,18 @@
 require_relative 'kunde'
 require_relative 'kredit'
 
-kunde1 = Kunde.new("Theo","Sommer")
-kunde1.name = "Theo Sonnenschein"
+kunde1 = Kunde.new("Theo Sommer")
+kunde1.name = "Theo sonnenschein"
 puts kunde1.name
 kunde1.gehalt = 2000
 kunde1.adresse = "Hermelinweg 11, 22159 Hamburg"
-kunde1.adresse1
+puts kunde1.adresse
+kunde1.name = kunde1.name.split(/ /).map(&:capitalize).join(" ")
+puts kunde1.name
+
+kredit1 = Kredit.new(kunde1)
+kredit1.jahresgehalt = 24000
+puts kredit1.kunde
+kredit1.kreditsumme = 5000
+kredit1.kreditvergabe
+kredit1.abzahlung(10)
