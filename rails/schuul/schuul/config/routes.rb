938dc8a0b5566460665f78f18176ba56d1aa35a4
--global <vossi.93@hotmail.de>
2015-11-26 15:01:53 +0100

    hi

--- rails/schuul/schuul/config/routes.rb
+++ rails/schuul/schuul/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "eventes#index"
+  root "events#index"
   resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
