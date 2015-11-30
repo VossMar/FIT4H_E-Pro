6e16982e8314dbaa2cec2ca12b78ed68ab2e3c7e
VossMar <vossi.93@hotmail.de>
2015-11-30 16:17:17 +0100

    sdkgh

--- rails/aufgaben/flix/app/models/movie.rb
+++ rails/aufgaben/flix/app/models/movie.rb
@@ -3,5 +3,7 @@ class Movie < ActiveRecord::Base
 		self.total_gross.blank? || self.total_gross < 50000000
 	end
 
-
+	def self.upcoming
+		where("release <= ?" , Time.now).order("release")
+	end
 end
